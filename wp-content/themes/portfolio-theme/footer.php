652db70c83d9717850807a8ea4a543a6ecdc4dd6
Rosa <rosa.morales.rojas@gmail.com>
2018-08-30 09:18:26 -0300

    first commit

---
+++ wp-content/themes/portfolio-theme/footer.php
@@ -0,0 +1,29 @@
+<?php if ( has_nav_menu( 'footer-menu' ) ) { ?>
+	<?php wp_nav_menu( array( 'theme_location' => 'footer-menu', 'container_class' => 'header_menu' ) ); ?>
+<?php } ?>
+
+<!-- Footer -->
+<div class="row my-5">
+  <div class="col-sm-6">
+      <div class="card-body">
+        <h1 class="titular__contactanos card-title">Como Encontrarnos</h1>
+        <span></span>
+      	<p><i class="fa fa-building fa-2x m-2 text-warning"></i>Oficina Virtual</p>
+      	<p><i class="fa fa-phone-square fa-2x m-2 text-warning"></i>Fono: + 56 9 0110545</p>
+       	<p><i class="fa fa-envelope fa-2x m-2 text-warning"></i>E-mail: rosa.morales.rojas@gmail.com</p>
+    </div>
+  </div>
+  <div class="col-sm-6">
+     <div class="titular__siguenos card-body">
+        <h1 class="card-title">Síguenos en Redes Sociales</h1>
+		<a href="https://www.facebook.com/effect.shot/" target="_blank"><span><i class="fa fa-facebook-f fa-4x m-5"></i></span></a>
+		<a href="https://twitter.com/effect_shot" target="_blank"><span><i class="fa fa-twitter fa-4x m-5"></i></span></a>
+		<a href="https://www.instagram.com/effect.shot/" target="_blank"><span><i class="fa fa-instagram fa-4x m-5"></i></span></a>
+      </div>
+  </div>
+</div>
+<!-- /Footer -->
+
+<?php wp_footer() ?>
+</body>
+</html>
---
+++ wp-content/themes/portfolio-theme/header.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo('charset') ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<?php get_template_part('_includes/iOS', 'icons') ?>
+	<?php wp_head() ?>
+</head>
+<body>
+
+	<!-- Navbar -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	<a class="navbar-brand" href="index.html">
+    <img src="<?php bloginfo('template_url') ?>/assets/images/logo-Effect-Shot.png" class="logo__navbar d-inline-block align-top" alt="logo"></a>
+  	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  	</button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.html">Home<span class="sr-only">(current)</span></a>
+      </li>
+
+		  <li class="nav-item dropdown">
+        <a class="nav-link" href="portafolio.html" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Portafolio</a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="fotografia.html">Fotografia</a>
+          <a class="dropdown-item" href="marca.html">Marca</a>
+          <a class="dropdown-item" href="web.html">Desarrollo Web</a>
+        </div>
+       </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="nosotros.html">Sobre Nosotros</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="contacto.html">Contacto</a>
+      </li>      
+    </ul>
+  </div>
+</nav>
+<!-- /Navbar -->
+
+<!-- Header -->
+<div class="img__header--jumbotron jumbotron jumbotron-fluid img-fluid display-4">
+  <div class="container">
+	<h2 class="titulo__header--jumbotron my-5 p-3 mb-2 text-white">Diseñamos tu negocio</h2>
+  </div>
+</div>
+<!-- /Header -->
+
+<?php if ( has_nav_menu( 'header-menu' ) ) { ?>
+	<?php wp_nav_menu( array( 'theme_location' => 'header-menu', 'container_class' => 'header_menu' ) ); ?>
+<?php } ?>
