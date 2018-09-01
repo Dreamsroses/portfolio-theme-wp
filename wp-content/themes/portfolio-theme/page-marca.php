f5895bb0abb93b7da1284b76769618abdd26f690
Rosa <rosa.morales.rojas@gmail.com>
2018-09-01 01:16:39 -0300

    se agregan tamaños de imagenes

--- wp-content/themes/portfolio-theme/page-marca.php
+++ wp-content/themes/portfolio-theme/page-marca.php
@@ -31,7 +31,7 @@
 		?>
 			
 		<div class="card">
-			<?php the_post_thumbnail() ?>
+			<?php the_post_thumbnail('mediana', array('class' => 'img-responsive')) ?>
 			<div class="card-body">
 				<h5 class="card-title"><?php the_title() ?></h5>
 				<p class="card-text"><?php the_excerpt() ?></p>
